72a2b3611fdbda669bc84403deb8b16c9710469f
Ndabarinze Mpano Maurice <ndabarinzempanomaurice@gmail.com>
2026-09-25 06:58:02 +0200

    Added styles

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -16,14 +16,15 @@ function Stopwatch(){
         return `00:00:00`
     }
     return(
-        <>
-        <div className="display">{stopwatchFormat()}</div>
-        <div className="buttons">
-            <button onClick={start} className="start-btn">Start</button>
-            <button onClick={reset} className="reset-btn">Reset</button>
-            <button onClick={stop} className="stop-btn">Stop</button>
+        <div className="stopwatch-container">
+            <div className="display">{stopwatchFormat()}</div>
+            <div className="buttons">
+                <button onClick={start} className="start-btn">Start</button>
+                <button onClick={reset} className="reset-btn">Reset</button>
+                <button onClick={stop} className="stop-btn">Stop</button>
+            </div>
         </div>
-        </>
+        
     );
 }
 
