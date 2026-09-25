b11f9f9614779db83eb1aa6e2c7419d84b4d87d9
Ndabarinze Mpano Maurice <ndabarinzempanomaurice@gmail.com>
2026-09-25 08:46:30 +0200

    Added pad zeros and finalize the app

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -38,6 +38,11 @@ function Stopwatch(){
         let seconds = Math.floor(elapsedTime / (1000) % 60);
         let milliseconds = Math.floor((elapsedTime % 1000) / 10);
 
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+
         return `${hours}:${minutes}:${seconds}:${milliseconds}`
     }
     return(
