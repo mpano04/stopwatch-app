a3c2a43e8907fbb95a2277545b833abfe4925854
Ndabarinze Mpano Maurice <ndabarinzempanomaurice@gmail.com>
2026-09-25 07:55:49 +0200

    Added functionality to the buttons

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,19 +1,44 @@
+import { useEffect, useState, useRef } from "react";
 
 
 function Stopwatch(){
 
+    const [isRunning, setIsRunnung] = useState(false);
+    const [elapsedTime,setElapsedTime] = useState(0);
+    const intervalIDRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+    
+        if(isRunning){
+            intervalIDRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            },10);
+        }
+
+        return () => {
+            clearInterval(intervalIDRef.current);
+        }
+    }, [isRunning]);
 
     function start(){
-
+        setIsRunnung(true);
+        startTimeRef.current = Date.now() - elapsedTime;
     }
     function reset(){
-
+        setElapsedTime(0);
+        setIsRunnung(false);
     }
     function stop(){
-
+        setIsRunnung(false);
     }
     function stopwatchFormat(){
-        return `00:00:00`
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+        return `${hours}:${minutes}:${seconds}:${milliseconds}`
     }
     return(
         <div className="stopwatch-container">
